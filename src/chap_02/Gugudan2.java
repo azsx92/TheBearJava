122d9e17e1152e1d1b81c7d95915c47602402559
azsx92 <azsx92@gmail.com>
2023-04-15 15:56:09 +0900

    피드백 요청사항2

--- src/chap_02/Gugudan2.java
+++ src/chap_02/Gugudan2.java
@@ -3,9 +3,14 @@ package chap_02;
 public class Gugudan2 {
     public static void main(String[] args) {
         for(int i = 2; i < 10 ; i++ ) {
-            for(int j = 1; j <= 9; j++) {
-                System.out.print(i + "*" + j + "=" + j*i + ",");
+            for(int j = 1; j < 10; j++) {
+                if(j % 9 == 1) {
+                    System.out.println();
+                } else {
+                    System.out.print(i  +  " * "  + j +  " = "  + j * i+", ");
+                }
             }
         }
     }
 }
+//diffchecker.com
